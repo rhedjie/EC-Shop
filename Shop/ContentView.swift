2809c31d729a1cf4a1e63477662852eb5f65e925
Rhedjie Cruz <cruzrhedjie@gmail.com>
2021-03-30 09:00:20 +0800

    Initial Commit

---
+++ Shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shop
+//
+//  Created by Rhedjie Cruz on 3/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Rhedjie Cruz on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
